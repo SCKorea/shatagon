f0c7043970ea8d608a4fa064c1aa6e7561859bba
marona <marona42@naver.com>
2021-02-20 01:24:38 +0900

    CCCV from Repository.HttpNetClient

---
+++ SCTools/SCTool_Redesigned/groceries/HttpNetClient.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Net;
+using System.Net.Http;
+
+namespace SCTool_Redesigned.groceries
+{
+    public static class HttpNetClient
+    {
+        private static readonly HttpClientHandler _clientHandler;
+        public static HttpClient Client { get; }
+        static HttpNetClient()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            _clientHandler = new HttpClientHandler();
+            //_clientHandler.UseProxy = App.Settings.UseHttpProxy;
+            Client = new HttpClient(_clientHandler);
+            Client.DefaultRequestHeaders.UserAgent.ParseAdd($"{App.Name}/{App.Version.ToString(3)}");
+            Client.Timeout = TimeSpan.FromMinutes(1);
+        }
+    }
+}
